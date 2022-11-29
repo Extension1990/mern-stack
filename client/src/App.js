5adc6a8566a47b9de21c8cb6c89dab2fb61a9b1d
Extension1990 <extensiontlhareseng@gmail.com>
2022-11-29 16:25:53 +0200

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import  Button  from './Button';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Button />
+    </div>
+  );
+}
+
+export default App;
